306c05aa7b6b29cfeff39ab3a464e21a2e80a733
kihno <stistyle13@gmail.com>
2023-03-27 17:54:23 -0700

    Style sign in and sign up pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :hosted_events, foreign_key: "host_id", class_name: "Event"
 
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
